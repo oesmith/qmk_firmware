854f6ce64e17107298e8e2a2b95e0eecde718812
Olly Smith <olly.smith@gmail.com>
2021-06-04 08:48:04 +0100

    Add RGB sliders on ADJUST layer. Tweak keymap a little bit.

--- keyboards/lily58/keymaps/ollysmith/keymap.c
+++ keyboards/lily58/keymaps/ollysmith/keymap.c
@@ -8,27 +8,27 @@ enum layer_number {
 };
 
 #define MY_QWERTY_MAP LAYOUT( \
-  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+  KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,       KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,       KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC, KC_RBRC, KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
                         KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,  KC_ENT,  MO(_RAISE), KC_MINS, KC_EQL \
 )
 
 // Note: LOWER and RAISE currently use the same map.
 #define MY_LOWER_RAISE_MAP LAYOUT( \
-  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,  \
+  KC_ESC,  _______, _______, _______, _______, _______,                                _______, _______, _______, _______, _______, _______,  \
   _______, _______, _______, _______, _______, _______,                                _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______,                                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, \
-  _______, RGB_TOG, RGB_MOD, _______, _______, _______, LGUI(KC_PSCR), LGUI(KC_PAUSE), _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, LGUI(KC_PSCR), LGUI(KC_PAUSE), KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, _______, \
                              _______, _______, _______, _______,       _______,        _______, _______, _______\
 )
 
 #define MY_ADJUST_MAP LAYOUT( \
-  _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,                           KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  _______, \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   _______, _______, _______, _______, _______, _______,                          _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______,                          KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX, \
-  _______, _______, _______, _______, _______, _______, _______, SGUI(KC_PAUSE), _______, _______, _______, _______, _______, _______, \
+  _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, SGUI(KC_PAUSE), _______, _______, _______, _______, _______, _______, \
                              _______, _______, _______, _______, _______,        _______, _______, _______\
 )
 
@@ -66,8 +66,34 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   return OLED_ROTATION_180;
 }
 
+const char lut[] = "0123456789ABCDEF";
+
+void render_hex_char(uint8_t num) {
+  oled_write_char(lut[num >> 4], false);
+  oled_write_char(lut[num & 0xf], false);
+}
+
+void render_slider(char label, uint8_t num) {
+  uint8_t pos = num >> 4;
+  oled_write_char(label, false);
+  for (uint8_t i = 0; i < 16 ; i++) {
+    oled_write_char(i == pos ? '|' : '-', false);
+  }
+  render_hex_char(num);
+  oled_write_ln("", false);
+}
+
 void oled_task_user(void) {
-  render_bongo_cat();
+  if (is_keyboard_master() && IS_LAYER_ON(_ADJUST)) {
+    oled_write("RGB Mode 0x", false);
+    render_hex_char(rgblight_get_mode());
+    oled_write_ln("", false);
+    render_slider('H', rgblight_get_hue());
+    render_slider('S', rgblight_get_sat());
+    render_slider('V', rgblight_get_val());
+  } else {
+    render_bongo_cat();
+  }
 }
 
 void suspend_power_down_user(void) {
