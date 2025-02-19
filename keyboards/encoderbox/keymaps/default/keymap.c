15a2237b62e4ff293b249fcdf4329e73c7f1f45e
Olly Smith <olly.smith@gmail.com>
2025-02-19 19:21:44 +0000

    Sim racing button box.

---
+++ keyboards/encoderbox/keymaps/default/keymap.c
@@ -0,0 +1,19 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(JS_0, JS_1, JS_2, JS_3)
+};
+
+// #if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = {
+        ENCODER_CCW_CW(JS_4, JS_5),
+        ENCODER_CCW_CW(JS_6, JS_7),
+        ENCODER_CCW_CW(JS_8, JS_9),
+        ENCODER_CCW_CW(JS_10, JS_11)
+    },
+};
+//#endif
